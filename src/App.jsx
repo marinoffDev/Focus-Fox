a2bd3008b114c8a504d762541ccbcdb76e32c1c7
marinoffDev <grffnmln@gmail.com>
2024-05-18 03:07:07 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <>
+    <div className='flex justify-center items-center h-screen'>
+      <Button>Hello World</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
