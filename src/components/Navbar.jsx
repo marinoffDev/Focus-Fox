d09601ddd3886fdeb7c6f1bbfcb8dc49d4a001ba
marinoffDev <grffnmln@gmail.com>
2024-05-21 03:46:25 +0300

    Add URL to logo. Add GitHub svg and URL. Move to separate components.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,17 @@
 import { Button } from "@/components/ui/button";
 import { ThemeProvider } from "@/components/ui/theme-provider"
 import { ModeToggle } from '@/components/ui/mode-toggle'
-import { Separator } from "@/components/ui/separator"
+import { Separator } from "@/components/ui/separator";
+import GitHubBtn from "@/components/ui/GitHubBtn";
+import Logo from "@/components/ui/Logo";
 
 export default function Navbar() {
   return (
     <>
       <nav className='relative mx-auto flex justify-center items-center space-x-4 mt-4'>
-        <div className="flex justify-center items-center">
-          <img className='w-10' src='https://res.cloudinary.com/grffn/image/upload/v1716236444/Focus-Fox/logo.png' alt='logo' />
-          <p className="font-bold">Focus Fox</p>
-        </div>
+        <Logo />
         <Button variant="secondary">How to use</Button>
-        <Button variant="secondary">GitHub</Button>
+        <GitHubBtn />
         <Button>Customize</Button>
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
           <ModeToggle />
---
+++ src/components/ui/Logo.jsx
@@ -0,0 +1,8 @@
+export default function Logo() {
+  return (
+    <a href="/Focus-Fox/" target="_self" className="flex justify-center items-center">
+        <img className='w-10' src='https://res.cloudinary.com/grffn/image/upload/v1716236444/Focus-Fox/logo.png' alt='Focus Fox Logo' />
+        <p className="font-bold">Focus Fox</p>
+    </a>
+  )
+}
