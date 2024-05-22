f9edcca7abc58db885b203ffa513fe9c9761e92d
marinoffDev <grffnmln@gmail.com>
2024-05-22 04:52:41 +0300

    Update package.json and add formatTime to utils

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
 }
+
+export function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+}
