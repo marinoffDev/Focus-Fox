01efb6abd197a7947b4f16174dc0cd78573c6432
marinoffDev <grffnmln@gmail.com>
2024-09-25 12:41:15 +0300

    fix gh-pages

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/Focus-Fox/",
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
