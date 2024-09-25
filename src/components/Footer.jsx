25970e54211b36f89b11e20325e3cdddc5f8a90e
marinoffDev <grffnmln@gmail.com>
2024-09-26 01:04:46 +0300

    footer styling changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,14 +6,13 @@ const year = new Date().getFullYear();
 export default function Footer({ scrollToHome }) {
   return (
     <>
-      <footer className="relative m-4 mx-auto flex max-w-2xl items-center justify-center space-x-4">
+      <footer className="relative m-4 pt-4 mx-auto flex max-w-2xl flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
         <Button variant="outline" onClick={scrollToHome}>
-          Home
+          Back To Top
         </Button>
         <Button variant="outline">Privacy Policy</Button>
         <Button variant="outline">Terms and Conditions</Button>
         <GitHubBtn variant="secondary" />
-        <div className="p-4 pt-2"></div>
       </footer>
       <p className="mb-4 w-full text-center text-xs">
         ©Focus Fox {year}. Code licensed under the{" "}
