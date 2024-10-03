a1ad7b7339baa8a138aa1872373436cfc8edbdb1
marinoffDev <grffnmln@gmail.com>
2024-10-03 10:37:31 +0300

    add react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./routes/App.jsx";
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/Focus-Fox/",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
