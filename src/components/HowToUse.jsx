dc30ce43ea235bc17b6668553ef42df8dfec593f
marinoffDev <grffnmln@gmail.com>
2024-10-11 11:04:42 +0300

    nav and page content styling/layout changes

--- src/components/HowToUse.jsx
+++ src/components/HowToUse.jsx
@@ -1,28 +1,28 @@
 export default function HowToUse() {
   return (
-    <div className="flex flex-col items-center justify-center py-16">
-      <h1 className="m-4 text-4xl font-black text-center md:text-5xl">Focus Fox</h1>
-      <h2 className="mb-10 text-xl font-bold text-muted-foreground text-center md:text-3xl md:mb-20">
+    <div className="flex flex-col items-center justify-center py-16 text-pretty container">
+      <h1 className="m-4 text-4xl font-black md:text-5xl">Focus Fox</h1>
+      <h2 className="mb-10 text-xl font-bold text-muted-foreground md:text-2xl md:mb-20">
         Your Go-To Online Timer for Enhanced Productivity
       </h2>
-      <h3 className="text-2xl font-extrabold text-center md:text-3xl">What is Focus Fox?</h3>
-      <p className="mb-10 w-11/12 pt-4 font-semibold text-muted-foreground text-justify md:w-7/12 md:mb-20">
+      <h3 className="text-2xl font-extrabold md:text-3xl">What is Focus Fox?</h3>
+      <p className="mb-10 w-11/12 pt-4 font-semibold text-muted-foreground md:w-8/12 md:mb-20">
         Focus Fox is an adaptable Pomodoro timer accessible on both desktop and
         mobile browsers. Its primary goal is to assist you in maintaining
         concentration on tasks like studying, writing, or programming. The
         design of this application is based on the Pomodoro Technique, a time
         management system developed by Francesco Cirillo.
       </p>
-      <h3 className="text-2xl font-extrabold text-center md:text-3xl">
+      <h3 className="text-2xl font-extrabold md:text-3xl">
         Understanding the Pomodoro Technique:
       </h3>
-      <p className="mb-10 w-11/12 pt-4 font-semibold text-muted-foreground text-justify md:w-7/12 md:mb-20">
+      <p className="mb-10 w-11/12 pt-4 font-semibold text-muted-foreground md:w-8/12 md:mb-20">
         {`The Pomodoro Technique is a method designed to improve efficiency in work and study environments. It involves using a timer to divide work into fixed intervals, typically 25 minutes, interspersed with brief rest periods. These intervals are referred to as "pomodoros," named after the tomato-shaped kitchen timer Cirillo utilized during his time at university.`}
       </p>
-      <h3 className="text-2xl font-extrabold text-center md:text-3xl">
+      <h3 className="text-2xl font-extrabold md:text-3xl">
         How to Use the Focus Fox Timer:
       </h3>
-      <ol className="m-4 w-11/12 list-decimal pl-6 font-semibold text-muted-foreground text-left md:w-7/12 md:pl-10">
+      <ol className="m-4 w-11/12 list-decimal pl-6 font-semibold text-muted-foreground md:w-8/12">
         <li>Customize the timer settings to fit your preferences.</li>
         <li>Choose a task to focus on.</li>
         <li>{`Start the Pomodoro timer and concentrate solely on your task for the set duration (default: 25 minutes).`}</li>
--- src/components/LegalPageLayout.jsx
+++ src/components/LegalPageLayout.jsx
@@ -1,36 +1,61 @@
+import { useState } from "react";
 import Logo from "@/components/ui/Logo";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import FooterCopyright from "./FooterCopyright";
 import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 export default function LegalPageLayout({navigateToRoute, navigateToRouteLabel, pageTitle, effectiveDateText, children}) {
+  const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
 
   return (
     <>
-      <div className="flex items-center justify-between p-4 relative mx-auto max-w-3xl">
-        <Logo />
-        <div>
-        <Button onClick={() => navigate(navigateToRoute)} variant="ghost">
-          {navigateToRouteLabel}
-        </Button>
-        <Button onClick={() => navigate("/Focus-Fox/")} variant="ghost">
-            Go Back Home
-        </Button>
+      <nav className="relative mx-auto max-w-3xl p-4">
+        <div className="flex items-center justify-between">
+          <Logo />
+          <div className="sm:hidden">
+            <button onClick={() => setIsOpen(!isOpen)} aria-label="Toggle menu">
+              {isOpen ? (
+                <FontAwesomeIcon icon={faTimes} className="w-6 h-6" />
+              ) : (
+                <FontAwesomeIcon icon={faBars} className="w-6 h-6" />
+              )}
+            </button>
+          </div>
+          <div className="hidden sm:flex space-x-4 items-center">
+            <Button onClick={() => navigate(navigateToRoute)} variant="ghost">
+              {navigateToRouteLabel}
+            </Button>
+            <Button onClick={() => navigate("/Focus-Fox/")} variant="ghost">
+              Go Back Home
+            </Button>
+          </div>
         </div>
-      </div>
+        {isOpen && (
+          <div className="sm:hidden flex flex-col items-center space-y-4 mt-4">
+            <Button onClick={() => navigate(navigateToRoute)} variant="ghost">
+              {navigateToRouteLabel}
+            </Button>
+            <Button onClick={() => navigate("/Focus-Fox/")} variant="ghost">
+              Go Back Home
+            </Button>
+          </div>
+        )}
+      </nav>
       <Separator className="m-auto block max-w-3xl" />
-      <div className="flex flex-col items-center justify-center m-auto py-4 max-w-3xl text-justify">
-        <div className="mb-10 pt-4 font-semibold text-muted-foreground">
+      <div className="flex flex-col items-center justify-center m-auto py-4 max-w-3xl text-pretty container">
+        <div className="my-4 font-semibold text-muted-foreground">
           <div className="flex flex-col justify-center items-center pb-2">
             <h1 className="text-4xl font-bold">{pageTitle}</h1>
           </div>
           <p className="py-2">Effective date: {effectiveDateText}</p>
           {children}
         </div>
-        <Separator className="m-auto block max-w-3xl" />
       </div>
+      <Separator className="m-auto block max-w-3xl mb-4" />
       <FooterCopyright />
     </>
   );
